d2c5a7c1ed53e34edacfcc7cd67490e0be6df10f
Yalung Kiranti <yalungkiranti46@gmail.com>
2025-11-01 20:28:09 +0545

    --Deleted uneccesary files

--- miscrits-bot-python/build_exe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Build single-file exe with PyInstaller
-pip install -r requirements.txt
-pip install pyinstaller
-pyinstaller --onefile --noconsole -p src src/app.py --name miscrits_bot
-echo Build complete. Check the 'dist' folder for miscrits_bot.exe
-pause
